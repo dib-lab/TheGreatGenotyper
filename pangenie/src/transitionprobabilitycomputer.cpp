eef6cb866fe1c2171a29b843b3b575dfa99d1786
Moustafa Shokrof <mshokrof@farm.ucdavis.edu>
2023-03-15 10:21:43 -0700

    add debug lines

--- pangenie/src/transitionprobabilitycomputer.cpp
+++ pangenie/src/transitionprobabilitycomputer.cpp
@@ -9,7 +9,7 @@ TransitionProbabilityComputer::TransitionProbabilityComputer(size_t from_variant
 {
 	assert(from_variant <= to_variant );
 	this->uniform = uniform;
-    this->phased = phased;
+	this->phased = phased;
 	// using same formula as in WhatsHap
 //	long double distance = (to_variant - from_variant) * 0.000001 * ((long double) recomb_rate) * 4.0L * effective_N;
 	long double distance = (to_variant - from_variant) * 0.000004L * ((long double) recomb_rate) * effective_N;
@@ -23,10 +23,12 @@ long double TransitionProbabilityComputer::compute_transition_prob(unsigned shor
 	if (this->uniform) {
 		return 1.0L;
 	} else if (this->phased){
+
 		// determine number of recombination events
 		unsigned int nr_events = 0;
 		if (path_id1 != path_id3) nr_events += 1;
 		if (path_id2 != path_id4) nr_events += 1;
+		//		cout<<"( "<<path_id1<<", "<<path_id2<<")"<<"\t( "<<path_id3<<", "<<path_id4<<")  -> "<<nr_events<<endl;
 		return this->probabilities[nr_events];
 	}
     else{
@@ -47,6 +49,7 @@ long double TransitionProbabilityComputer::compute_transition_prob(unsigned shor
             if(path_id2 != path_id3 and path_id2 != path_id4  )
                 nr_events+=1;
         }
+	//cout<<"( "<<path_id1<<", "<<path_id2<<")"<<"\t( "<<path_id3<<", "<<path_id4<<")  -> "<<nr_events<<endl;
         return this->probabilities[nr_events];
     }
 }
