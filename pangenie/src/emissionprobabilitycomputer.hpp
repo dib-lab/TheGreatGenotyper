231d509a2539fa082fdb181c67abf204abddd711
Mostafa <mostafa.shokrof@gmail.com>
2023-04-04 09:49:05 -0700

    loading the precacluated emission but now it is hardcoded for my case

--- pangenie/src/emissionprobabilitycomputer.hpp
+++ pangenie/src/emissionprobabilitycomputer.hpp
@@ -40,6 +40,7 @@ public:
     /**
     * @param uniquekmers all unique kmers for this position
      **/
+    EmissionProbabilities();
     EmissionProbabilities(SamplesDatabase* samples,unsigned  nr_variants);
     /** get emission probability for a state in the HMM **/
     long double get_emission_probability(unsigned variantID,unsigned sampleID, unsigned char allele_id1, unsigned char allele_id2) const;
